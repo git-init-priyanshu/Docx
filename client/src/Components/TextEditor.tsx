37a09cf2309d59a2da213100a71513cfe6c811f0
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-07-22 17:29:03 +0530

    Added socket.io to the server and connected server to the client

--- client/src/Components/TextEditor.tsx
+++ client/src/Components/TextEditor.tsx
@@ -1,29 +1,16 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Quill from "quill";
+
 import "quill/dist/quill.snow.css";
+import { socket } from "../socket";
+import { TOOLBAR_OPTIONS } from "./ToolbarOptions";
 
-const TOOLBAR_OPTIONS = [
-  [{ font: [] }],
-  ["bold", "italic", "underline", "strike"], // toggled buttons
-  ["blockquote", "code-block"],
+export default function TextEditor() {
+  // const [isConnected, setIsConnected] = useState(socket.connected);
 
-  [{ header: 1 }, { header: 2 }], // custom button values
-  [{ list: "ordered" }, { list: "bullet" }],
-  [{ script: "sub" }, { script: "super" }], // superscript/subscript
-  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
-  [{ direction: "rtl" }], // text direction
-
-  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
-  [{ header: [1, 2, 3, 4, 5, 6, false] }],
-
-  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
-  [{ align: [] }],
-
-  ["clean"], // remove formatting button
-];
-
-const TextEditor = () => {
   useEffect(() => {
+    socket.connect();
+
     new Quill("#container", {
       theme: "snow",
       modules: { toolbar: TOOLBAR_OPTIONS },
@@ -34,10 +21,10 @@ const TextEditor = () => {
       if (rootElement) {
         rootElement.innerHTML = "<div id='container'></div>";
       }
+
+      socket.disconnect();
     };
   }, []);
 
   return <div id="container"></div>;
-};
-
-export default TextEditor;
+}
