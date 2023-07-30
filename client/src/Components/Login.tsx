4db7f58349ea4c6bdf5af5df30adbc82555d670e
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-07-30 20:00:36 +0530

    Added functionality to Login, Signup page

--- client/src/Components/Login.tsx
+++ client/src/Components/Login.tsx
@@ -1,23 +1,54 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  interface userState {
+    email: string;
+    password: string;
+  }
+  const [userState, setUserState] = useState<userState>({
+    email: "",
+    password: "",
+  });
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserState({ ...userState, [e.target.name]: [e.target.value] });
+  };
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(userState);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <div className="form-title">Login</div>
       <div className="group">
-        <input type="email" />
+        <input
+          type="email"
+          name="email"
+          className={`${userState.email !== "" ? "used" : ""}`}
+          value={userState.email}
+          onChange={handleOnChange}
+        />
         <span className="highlight"></span>
         <span className="bar"></span>
         <label>Email</label>
       </div>
       <div className="group">
-        <input type="password" />
+        <input
+          type="password"
+          name="password"
+          className={`${userState.password !== "" ? "used" : ""}`}
+          value={userState.password}
+          onChange={handleOnChange}
+        />
         <span className="highlight"></span>
         <span className="bar"></span>
         <label>Password</label>
       </div>
-      <button type="button" className="button buttonBlue">
-        Subscribe
+      <button type="submit" className="button buttonBlue">
+        Login
         <div className="ripples buttonRipples">
           <span className="ripplesCircle"></span>
         </div>
--- client/src/Components/Signup.tsx
+++ client/src/Components/Signup.tsx
@@ -1,29 +1,68 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export default function Signup() {
+  interface userState {
+    email: string;
+    password: string;
+    confirm_password: string;
+  }
+  const [userState, setUserState] = useState<userState>({
+    email: "",
+    password: "",
+    confirm_password: "",
+  });
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUserState({ ...userState, [e.target.name]: [e.target.value] });
+  };
+
+  const handleOnSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(userState);
+  };
+
   return (
-    <form>
+    <form onSubmit={handleOnSubmit}>
       <div className="form-title">Signup</div>
       <div className="group">
-        <input type="email" />
+        <input
+          type="email"
+          name="email"
+          className={`${userState.email !== "" ? "used" : ""}`}
+          value={userState.email}
+          onChange={handleOnChange}
+        />
         <span className="highlight"></span>
         <span className="bar"></span>
         <label>Email</label>
       </div>
       <div className="group">
-        <input type="password" />
+        <input
+          type="password"
+          name="password"
+          className={`${userState.password !== "" ? "used" : ""}`}
+          value={userState.password}
+          onChange={handleOnChange}
+        />
         <span className="highlight"></span>
         <span className="bar"></span>
         <label>Password</label>
       </div>
       <div className="group">
-        <input type="password" />
+        <input
+          type="password"
+          name="confirm_password"
+          className={`${userState.confirm_password !== "" ? "used" : ""}`}
+          value={userState.confirm_password}
+          onChange={handleOnChange}
+        />
         <span className="highlight"></span>
         <span className="bar"></span>
         <label>Confirm Password</label>
       </div>
-      <button type="button" className="button buttonBlue">
-        Subscribe
+      <button type="submit" className="button buttonBlue">
+        Submit
         <div className="ripples buttonRipples">
           <span className="ripplesCircle"></span>
         </div>
