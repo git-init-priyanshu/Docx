2b9c5da76e53bbe84f8200c8add5741bc00ed7d5
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-10-15 21:07:57 +0530

    fix "Cannot access URL before initialization"

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-import { URL } from "./App";
+const URL: string = import.meta.env.DEV
+  ? import.meta.env.VITE_DEV_URL
+  : import.meta.env.VITE_PROD_URL;
 
 const SOCKET_URL = `http://${URL}`;
 export const socket = io(SOCKET_URL, { autoConnect: true});
