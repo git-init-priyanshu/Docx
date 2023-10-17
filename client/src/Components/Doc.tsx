e7d8a986bad4a37a806adfab446fa0b81cd36b39
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-10-17 19:09:22 +0530

    Fix: Toolbar issue

--- client/src/Components/Doc.tsx
+++ client/src/Components/Doc.tsx
@@ -77,8 +77,7 @@ export default function Doc() {
        * This was resulting in a blank page appearing when navigating back to the home page.
        * The new solution now only removes the necessary element and does not completely change the entire root element.
        */
-      rootElement?.children[0].children[0].remove();
-
+      rootElement?.children[0].remove();
       socket.disconnect();
       toggleConnected();
     };
