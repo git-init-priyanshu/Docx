0794e906358ed3e70b5470f3230a7149910a31f9
Priyanshu <bartwalpriyanshu00@gmail.com>
2023-12-08 12:57:12 +0530

    UI changes for Login & Signup pages.

--- client/src/Components/Login.tsx
+++ client/src/Components/Login.tsx
@@ -1,11 +1,24 @@
 import { useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useMutation, useLazyQuery } from "@apollo/client";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import { FIND_USER_QUERY } from "../Graphql/queries";
 import { LOGIN_MUTATION } from "../Graphql/mutations";
 
 export default function Login() {
+  const defaultTheme = createTheme();
+
   const navigate = useNavigate();
 
   interface userState {
@@ -35,11 +48,11 @@ export default function Login() {
   }, [findUser, token]);
   if (data?.findUser) navigate("/home");
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserState({ ...userState, [e.target.name]: e.target.value });
   };
 
-  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     login({
@@ -67,41 +80,110 @@ export default function Login() {
   // if (loginError) return window.alert(loginError.message);
 
   return (
-    <form onSubmit={handleOnSubmit} id="loginForm">
-      <div className="form-title">Login</div>
-      <div className="group">
-        <input
-          type="email"
-          name="email"
-          className={`${userState.email !== "" ? "used" : ""}`}
-          value={userState.email}
-          onChange={handleOnChange}
-        />
-        <span className="highlight"></span>
-        <span className="bar"></span>
-        <label>Email</label>
-      </div>
-      <div className="group">
-        <input
-          type="password"
-          name="password"
-          className={`${userState.password !== "" ? "used" : ""}`}
-          value={userState.password}
-          onChange={handleOnChange}
-        />
-        <span className="highlight"></span>
-        <span className="bar"></span>
-        <label>Password</label>
-      </div>
-      <button type="submit" className="button buttonBlue">
-        Login
-        <div className="ripples buttonRipples">
-          <span className="ripplesCircle"></span>
-        </div>
-      </button>
-      <p className="footer">
-        Don't have an account? <Link to={"/signup"}>Signup</Link>
-      </p>
-    </form>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Log In
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              onChange={handleChange}
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+              onChange={handleChange}
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              sx={{ mt: 3, mb: 2 }}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/signup" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+    // <form onSubmit={handleOnSubmit} id="loginForm">
+    //   <div className="form-title">Login</div>
+    //   <div className="group">
+    //     <input
+    //       type="email"
+    //       name="email"
+    //       className={`${userState.email !== "" ? "used" : ""}`}
+    //       value={userState.email}
+    //       onChange={handleOnChange}
+    //     />
+    //     <span className="highlight"></span>
+    //     <span className="bar"></span>
+    //     <label>Email</label>
+    //   </div>
+    //   <div className="group">
+    //     <input
+    //       type="password"
+    //       name="password"
+    //       className={`${userState.password !== "" ? "used" : ""}`}
+    //       value={userState.password}
+    //       onChange={handleOnChange}
+    //     />
+    //     <span className="highlight"></span>
+    //     <span className="bar"></span>
+    //     <label>Password</label>
+    //   </div>
+    //   <button type="submit" className="button buttonBlue">
+    //     Login
+    //     <div className="ripples buttonRipples">
+    //       <span className="ripplesCircle"></span>
+    //     </div>
+    //   </button>
+    //   <p className="footer">
+    //     Don't have an account? <Link to={"/signup"}>Signup</Link>
+    //   </p>
+    // </form>
   );
 }
--- client/src/Components/Signup.tsx
+++ client/src/Components/Signup.tsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { useMutation } from "@apollo/client";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import { SIGNUP_MUTATION } from "../Graphql/mutations";
 
 export default function Signup() {
+  const defaultTheme = createTheme();
+
   const navigate = useNavigate();
 
   interface userState {
@@ -22,11 +35,11 @@ export default function Signup() {
   const [signup, { data: signupData, error: signupError }] =
     useMutation(SIGNUP_MUTATION);
 
-  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserState({ ...userState, [e.target.name]: e.target.value });
   };
 
-  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     // Todo: password === confirm_password
@@ -55,53 +68,159 @@ export default function Signup() {
   }
 
   return (
-    <form onSubmit={handleOnSubmit} id="signupForm">
-      <div className="form-title">Signup</div>
-      <div className="group">
-        <input
-          type="email"
-          name="email"
-          className={`${userState.email !== "" ? "used" : ""}`}
-          value={userState.email}
-          onChange={handleOnChange}
-        />
-        <span className="highlight"></span>
-        <span className="bar"></span>
-        <label>Email</label>
-      </div>
-      <div className="group">
-        <input
-          type="password"
-          name="password"
-          className={`${userState.password !== "" ? "used" : ""}`}
-          value={userState.password}
-          onChange={handleOnChange}
-        />
-        <span className="highlight"></span>
-        <span className="bar"></span>
-        <label>Password</label>
-      </div>
-      <div className="group">
-        <input
-          type="password"
-          name="confirm_password"
-          className={`${userState.confirm_password !== "" ? "used" : ""}`}
-          value={userState.confirm_password}
-          onChange={handleOnChange}
-        />
-        <span className="highlight"></span>
-        <span className="bar"></span>
-        <label>Confirm Password</label>
-      </div>
-      <button type="submit" className="button buttonBlue">
-        Submit
-        <div className="ripples buttonRipples">
-          <span className="ripplesCircle"></span>
-        </div>
-      </button>
-      <p className="footer">
-        Already have an account? <Link to={"/"}>Login</Link>
-      </p>
-    </form>
+    <ThemeProvider theme={defaultTheme}>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <Box
+          sx={{
+            marginTop: 8,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={handleSubmit}
+            noValidate
+            sx={{ mt: 1 }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="given-name"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="family-name"
+                />
+              </Grid>
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                // autoComplete="new-password"
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="confirm_password"
+                label="Confirm Password"
+                type="password"
+                id="password"
+                // autoComplete="current-password"
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid>
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Sign Up
+              </Button>
+            </Grid>
+            <Grid container justifyContent="flex-end">
+              <Grid item>
+                <Link href="/" variant="body2">
+                  {"Already have an account? Log In"}
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </ThemeProvider>
+    //    <form onSubmit={handleOnSubmit} id="signupForm">
+    //    <div className="form-title">Signup</div>
+    //    <div className="group">
+    //      <input
+    //        type="email"
+    //        name="email"
+    //        className={`${userState.email !== "" ? "used" : ""}`}
+    //        value={userState.email}
+    //        onChange={handleOnChange}
+    //      />
+    //      <span className="highlight"></span>
+    //      <span className="bar"></span>
+    //      <label>Email</label>
+    //    </div>
+    //    <div className="group">
+    //      <input
+    //        type="password"
+    //        name="password"
+    //        className={`${userState.password !== "" ? "used" : ""}`}
+    //        value={userState.password}
+    //        onChange={handleOnChange}
+    //      />
+    //      <span className="highlight"></span>
+    //      <span className="bar"></span>
+    //      <label>Password</label>
+    //    </div>
+    //    <div className="group">
+    //      <input
+    //        type="password"
+    //        name="confirm_password"
+    //        className={`${userState.confirm_password !== "" ? "used" : ""}`}
+    //        value={userState.confirm_password}
+    //        onChange={handleOnChange}
+    //      />
+    //      <span className="highlight"></span>
+    //      <span className="bar"></span>
+    //      <label>Confirm Password</label>
+    //    </div>
+    //    <button type="submit" className="button buttonBlue">
+    //      Submit
+    //      <div className="ripples buttonRipples">
+    //        <span className="ripplesCircle"></span>
+    //      </div>
+    //    </button>
+    //    <p className="footer">
+    //      Already have an account? <Link to={"/"}>Login</Link>
+    //    </p>
+    //  </form>
   );
 }
