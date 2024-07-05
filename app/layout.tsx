4ec0788e57745fb784f6ccb649a55ce3d34ea9f8
Priyanshu <bartwalpriyanshu00@gmail.com>
2024-07-05 11:49:11 +0530

    Initialized React Query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import { Inter } from "next/font/google";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import ReactQueryProvider from "./ReactQueryProvider";
 import "./globals.css";
-
-import { cn } from "@/lib/utils"
 import Header from "@/components/Header";
+import { cn } from "@/lib/utils";
 
-const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-})
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Docx",
@@ -21,14 +20,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={cn(
-        "min-h-screen bg-background font-sans antialiased",
-        fontSans.variable
-      )}>
-        <Header />
-        {children}
-      </body>
-    </html>
+    <ReactQueryProvider>
+      <html lang="en">
+        <body className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          inter.className
+        )}>
+          <Header />
+          {children}
+          <ReactQueryDevtools initialIsOpen={false} />
+        </body>
+      </html>
+    </ReactQueryProvider>
   );
 }
