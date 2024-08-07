5185d51fa55afbc6a72dc9fb41a2955801964a3e
Priyanshu <bartwalpriyanshu00@gmail.com>
2024-08-07 15:25:35 +0530

    Test

--- lib/auth.ts
+++ lib/auth.ts
@@ -75,17 +75,17 @@ export const authOptions: NextAuthOptions = {
       return session
     }
   },
-  // cookies: {
-  //   sessionToken: {
-  //     name: 'token',
-  //     options: {
-  //       httpOnly: true,
-  //       sameSite: 'lax',
-  //       path: '/',
-  //       secure: true
-  //     }
-  //   }
-  // },
+  cookies: {
+    sessionToken: {
+      name: 'next-auth.session-token',
+      options: {
+        httpOnly: true,
+        sameSite: 'lax',
+        path: '/',
+        secure: true
+      }
+    }
+  },
   pages: {
     signIn: "/signin",
   },
