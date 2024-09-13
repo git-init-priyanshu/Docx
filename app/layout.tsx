6dde744c48127f852b538507f29d3b881c1eef47
Priyanshu <bartwalpriyanshu00@gmail.com>
2024-09-13 23:17:24 +0530

    Vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
@@ -33,6 +34,7 @@ export default function RootLayout({
             )}
           >
             <Toaster />
+            <Analytics />
             {children}
             <ReactQueryDevtools initialIsOpen={false} />
           </body>
